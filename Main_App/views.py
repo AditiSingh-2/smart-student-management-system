17ca627a1571b6884ec1bf39de326e606a80208e
AditiSingh-2 <aditisingh11699@gmail.com>
2026-03-22 22:39:01 +0530

    Update views.py

--- Main_App/views.py
+++ Main_App/views.py
@@ -14,21 +14,25 @@ def loginpage(request):
 def loginuser(request):
     if request.method != "POST":
         return HttpResponse("<h2>Method not allowed</h2>")
-    else : 
-        user = EmailAuth.authenticate(request,username=request.POST.get('email'),password=request.POST.get('password'))
-        if user!=None:
-            login(request,user)
-            if user.user_type=='1':
-                return HttpResponseRedirect('/adminhome')
-            elif user.user_type=='2':
-                return HttpResponseRedirect('/teacherhome')
-            elif user.user_type=='3' :
-                return HttpResponseRedirect('/studenthome')
-            else :
-                return HttpResponse("Invalid user type")
-        else :
-            messages.error(request,"Invalid login details..!")
-            return HttpResponseRedirect("/")  # correct
+    else:
+        user = EmailAuth.authenticate(
+            request,
+            username=request.POST.get('email'),
+            password=request.POST.get('password')
+        )
+        if user is not None:
+            login(request, user)
+            if user.user_type == '1':
+                return HttpResponseRedirect('/adminhome/')
+            elif user.user_type == '2':
+                return HttpResponseRedirect('/teacherhome/')
+            elif user.user_type == '3':
+                return HttpResponseRedirect('/studenthome/')
+            else:
+                return HttpResponse(f"Invalid user type: {user.user_type}")
+        else:
+            messages.error(request, "Invalid login details..!")
+            return HttpResponseRedirect("/")
 def logoutuser(request):
     logout(request)
     return HttpResponseRedirect('/')
